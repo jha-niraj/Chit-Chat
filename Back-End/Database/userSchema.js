cebcb9e87b2b35837eedbcf050feb37954c49499
jha-niraj <jhaniraj45@gmail.com>
2024-05-12 21:10:15 +0530

    Added the .env file in the Project

--- Back-End/Database/userSchema.js
+++ Back-End/Database/userSchema.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://jhaniraj45:EM26awsTr4nB5aik@cluster-1.vikopuu.mongodb.net/user");
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
 
 const userSchema = new mongoose.Schema({
     fullname: {
